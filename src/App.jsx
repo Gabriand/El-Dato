c6b2caba01612be443db0b862019aa524a4b7e45
Gabriand <gabrielrivera11a@gmail.com>
2026-03-23 17:31:38 -0500

    feat: setup inicial de react con vite y tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-2xl font-bold">
+                Hola, este es el inicio del proyecto
+            </h1>
+            <span>😎</span>
+        </div>
+    );
+}
+
+export default App;
