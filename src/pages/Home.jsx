c9fa35ab2b59896c967d2d6c58a5337d70c08468
Gabriand <gabrielrivera11a@gmail.com>
2026-03-23 21:15:49 -0500

    ui: lista disponible y ajustes en las variables

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,28 @@
 export default function Home() {
-    return <main className="p-4">Home</main>;
+    return (
+        <main>
+            <div className="mx-4 overflow-x-auto">
+                <ul className="flex text-primary gap-3 min-w-max sm:text-base lg:text-lg">
+                    <li className="shrink-0 border-2 border-surface px-5 py-2 rounded-full hover:text-bg hover:bg-primary">
+                        Todos
+                    </li>
+                    <li className="shrink-0 border-2 border-surface px-5 py-2 rounded-full hover:text-bg hover:bg-primary">
+                        Víveres
+                    </li>
+                    <li className="shrink-0 border-2 border-surface px-5 py-2 rounded-full hover:text-bg hover:bg-primary">
+                        Frutas
+                    </li>
+                    <li className="shrink-0 border-2 border-surface px-5 py-2 rounded-full hover:text-bg hover:bg-primary">
+                        Carnes
+                    </li>
+                    <li className="shrink-0 border-2 border-surface px-5 py-2 rounded-full hover:text-bg hover:bg-primary">
+                        Lacteos
+                    </li>
+                    <li className="shrink-0 border-2 border-surface px-5 py-2 rounded-full hover:text-bg hover:bg-primary">
+                        Aseo
+                    </li>
+                </ul>
+            </div>
+        </main>
+    );
 }
